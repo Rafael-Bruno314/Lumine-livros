3c0c454b744d35c6879aee634164a19d89fabe67
Rafael Bruno <rafaelbruno.cunha@gmail.com>
2024-08-16 02:25:51 -0300

    Update conecta.php

--- Class para servidor/conecta.php
+++ Class para servidor/conecta.php
@@ -1,8 +1,8 @@
 <?php
 $servidor = "localhost";
-$usuario = "id4936723_lumine";
-$senha = "antimateria";
-$banco = "id4936723_lumine";
+$usuario = "xxxxxxxxxxxxxxxxxxxxx";
+$senha = "xxxxxxxxxxxxxx";
+$banco = "xxxxxxxxxxxxxxxxxxx";
 
 $con = mysqli_connect($servidor, $usuario, $senha, $banco);
 ?>
